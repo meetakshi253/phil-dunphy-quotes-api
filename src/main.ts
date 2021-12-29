10e340ce2277384cb4804128c357c2d206b4e3b0
meetakshi253 <meetakshisty4@gmail.com>
2021-12-29 23:13:33 +0530

    Use routes instead of subdomains

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import express, { Application, Request, Response } from "express";
 import lyrics from "src/lyrics.json";
 
-var subdomain = require("express-subdomain");
 var router = express.Router();
 
 const app: Application = express();
@@ -10,7 +9,6 @@ const PORT = process.env.PORT || 3000;
 // Body parsing Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(subdomain("api", router));
 
 //route
 app.get("/", async (req: Request, res: Response) => {
@@ -18,11 +16,18 @@ app.get("/", async (req: Request, res: Response) => {
 });
 
 //api subdomain route
-router.get("/", async (req: Request, res: Response) => {
+app.get("/api", async (req: Request, res: Response) => {
   const lyric = lyrics[Math.floor(Math.random() * lyrics.length)];
   res.header("Content-Type", "application/json");
   return res.status(200).send({
     message: lyric,
+  });
+});
+
+app.get("/api/all", async (req: Request, res: Response) => {
+  res.header("Content-Type", "application/json");
+  return res.status(200).send({
+    message: lyrics,
   });
 });
 
