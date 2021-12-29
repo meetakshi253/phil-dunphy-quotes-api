e676a5103980c0e42a05069f57750d36ac41e79c
meetakshi253 <meetakshisty4@gmail.com>
2021-12-29 21:03:23 +0530

    Minor fix

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import express, { Application, Request, Response } from "express";
 
 const app: Application = express();
-const port = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Body parsing Middleware
 app.use(express.json());
@@ -25,8 +25,8 @@ app.all("*", async (req: Request, res: Response, next)=>
 })
 
 try {
-    app.listen(port, (): void => {
-        console.log(`Connected successfully on port ${port}`);
+    app.listen(PORT, (): void => {
+        console.log(`Connected successfully on port ${PORT}`);
     });
 } catch (error) {
     console.error("Error occured:");
