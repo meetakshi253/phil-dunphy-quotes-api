be707b5efcd7451b7b324fa521b662633f50749d
meetakshi253 <meetakshisty4@gmail.com>
2021-12-29 22:43:53 +0530

    Formatting and minor tasks

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,8 +1,8 @@
 module.exports = {
-    semi: true,
-    trailingComma: "es5",
-    singleQuote: false,
-    useTabs: false,
-    tabWidth: 2,
-    printWidth: 80,
-  };
+  semi: true,
+  trailingComma: "es5",
+  singleQuote: false,
+  useTabs: false,
+  tabWidth: 2,
+  printWidth: 80,
+};
