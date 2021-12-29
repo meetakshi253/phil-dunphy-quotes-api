747f89e7555be02c47107342cff4e1eaaef4759d
meetakshi253 <meetakshisty4@gmail.com>
2021-12-29 22:43:30 +0530

    Set up API subdomain

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,8 @@
 import express, { Application, Request, Response } from "express";
+import lyrics from "src/lyrics.json";
+
+var subdomain = require("express-subdomain");
+var router = express.Router();
 
 const app: Application = express();
 const PORT = process.env.PORT || 3000;
@@ -6,28 +10,29 @@ const PORT = process.env.PORT || 3000;
 // Body parsing Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(subdomain("api", router));
 
-app.get(
-    "/",
-    async (req: Request, res: Response): Promise<Response> => {
-        return res.status(200).send({
-            message: "Welcome to Taylor Swift Lyrics API. Developed by Meetakshi Setiya",
-        });
-    }
-);
+//route
+app.get("/", async (req: Request, res: Response) => {
+  console.log("frontend");
+});
 
-//all unhandled routes
-app.all("*", async (req: Request, res: Response, next)=>
-{
-    return res.status(404).send({
-        message: "Route not found."
-    })
-})
+//api subdomain route
+router.get("/", async (req: Request, res: Response) => {
+  const lyric = lyrics[Math.floor(Math.random() * lyrics.length)];
+  res.header("Content-Type", "application/json");
+  return res.status(200).send({
+    message: lyric,
+  });
+});
 
-try {
-    app.listen(PORT, (): void => {
-        console.log(`Connected successfully on port ${PORT}`);
-    });
-} catch (error) {
-    console.error("Error occured:");
-}
+//unhandled routes
+app.all("*", async (req: Request, res: Response) => {
+  return res.status(404).send({
+    message: "Route not found.",
+  });
+});
+
+app.listen(PORT, (): void => {
+  console.log(`Connected successfully on port ${PORT}`);
+});
